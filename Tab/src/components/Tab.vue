6ef28c5771b810e13e96d04c1506bf04102b4ff5
kevin.maciel.704 <kevin.maciel.704@imd.local>
2022-04-13 10:26:54 -0300

    First commit

---
+++ Tab/src/components/Tab.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+    export interface TabProps {
+    tabNum: number  // enunciado da questão
+    tabText: string  // opções da questão
+  }
+
+  // Eventos do componente.
+  export interface TabEvents {
+    (e: 'onClick', val: string): void // quando o usuário selecionar uma opção, passa seu índice
+  }
+
+  // define as propriedades e os eventos
+  defineProps<TabProps>()
+  defineEmits<TabEvents>()
+</script>
+
+<template>
+  <button @click="$emit('onClick', tabText)">Tab {{ tabNum }}</button> 
+</template>
+
+<style scoped>
+  h2 {
+    font-weight: normal;
+    font-size: 20pt;
+    margin-top: 0;
+  }
+
+  button {
+    margin-right: 10px;
+    background-color: #ccc;
+    border: 1px solid rgb(118, 118, 118);
+    border-radius: 3px;
+    padding: 2px 5px;
+  }
+
+  .selected {
+    font-weight: bold;
+    background-color: #aaa;
+  }
+</style>
